b488baf5ec0e79b76f1a2845f021b559c689df14
gustavo9601 <tavo9601@gmail.com>
2021-08-29 23:22:13 -0500

    init views based in classes

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+from django.views.generic.base import TemplateView
 
-def sample(request):
-    return render(request, "core/sample.html")
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+    # Metodo que permite retornar el contexto
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Mi super Web GM'
+        return context
+
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
+
+    # Sobreescribiendo la respuesta del la vista basada en clase
+    def get(self, request, *args, **kwargs):
+        return render(request=request, template_name=self.template_name, context={'title': 'Samplee'})
