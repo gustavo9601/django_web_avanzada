24e96d1ceb58bb9e5c3cafba1dcab9f682b35400
gustavo9601 <tavo9601@gmail.com>
2021-09-05 19:06:35 -0500

    add signal to create perfil adter create an user

--- webplayground/registration/models.py
+++ webplayground/registration/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -8,3 +11,20 @@ class Profile(models.Model):
     link = models.URLField(null=True, blank=True, max_length=255)
     # Solo puede haber un perfil por cada usuario
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+"""
+Signals => Las señales son eventos a ejecutar cuando algo ocurra en un modelo
+
+@receiver(tipo_de_señal, sender=Modelo a escuchar el evento)
+"""
+
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    # Se valida si viene created en kwargs, solo se llama cuando se crea por lo tanto las demas veces
+    # que se modifiquen el modelo no se enviara y no se actualizara
+    if kwargs.get('created', False):
+        Profile.objects.get_or_create(instance)
+        print("Se creo un usuario y perfil por señal")
+
